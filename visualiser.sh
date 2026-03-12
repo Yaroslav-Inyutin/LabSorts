c1ba0dd6b01f1ffb8192cd3aa4698ac1c87e404a
Yaroslav-Inyutin <yarikfonarik121@gmail.com>
2026-03-12 15:16:55 +0300

    Script corrected, video made

--- visualiser.sh
+++ visualiser.sh
@@ -2,9 +2,22 @@
 rm -rf sorts
 mkdir sorts
 ./sorts.out
-for file in sorts/*.csv
-do
-	./visualiser "$(basename "$file")"
+
+images=()
+for image in images/*.jpg; do
+	images+=("$(basename "$image")")
 done
 
+files=()
+for file in sorts/*.csv; do
+	files+=("$(basename "$file")")
+done
 
+j=0
+for i in "${!files[@]}"; do
+	./visualiser "${files[$i]}" "${images[$j]}"
+	((j++))
+	if [ $j -eq ${#images[@]} ]; then
+		j=0
+	fi
+done
